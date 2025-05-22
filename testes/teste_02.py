d1f2d86c92c0f0de1b57848b2a0f25ddbfb515cb
MatheusY534 <matheus.yassuda.63049@a.fecaf.com.br>
2025-05-22 20:43:26 -0300

    Update teste_02.py

--- testes/teste_02.py
+++ testes/teste_02.py
@@ -1,1 +1,20 @@
+def soma(a, b):
+    return a + b
 
+import unittest
+
+class TestSoma(unittest.TestCase):
+    def test_soma_positivos(self):
+        self.assertEqual(soma(2, 3), 5)  # 2 + 3 = 5
+
+    def test_soma_negativos(self):
+        self.assertEqual(soma(-1, -1), -2)  # -1 + (-1) = -2
+
+    def test_soma_mista(self):
+        self.assertEqual(soma(5, -3), 2)  # 5 + (-3) = 2
+
+    def test_soma_zero(self):
+        self.assertEqual(soma(0, 0), 0)  # 0 + 0 = 0
+
+if __name__ == '__main__':
+    unittest.main(argv=[''], exit=False)
